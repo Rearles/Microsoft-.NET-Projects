2c21fb5c1a2d66909a758f7873752b75e3960378
Ryan Earles <ryanjayearles@gmail.com>
2021-07-30 17:47:34 -0600

    this is an attempt

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace myApp
 {
     class Program
     {
+        Dictionary<string, string> names = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
+
+            bool cardnum = false;
+            while (cardnum == false) {
+                Console.WriteLine("Enter your credit card number or debit card number");
+                string ccnum = Console.ReadLine();
+                if (ccnum.IndexOf(4) == 0) {
+                Console.WriteLine("Your card is a Visa correct?");
+                string answer = Console.ReadLine();                
+                if (answer == "Yes" || answer == "yes") {
+                    Console.WriteLine("Thank you for confirming for me");
+                    cardnum = true;
+                } 
+                else if (answer == "No" || answer == "no") {
+                    Console.WriteLine("You entered the wrong information into the system. Please try again.");
+                } else {
+                    Console.WriteLine("You entered incorrect characters, try again");
+                }
+            }
+            
+            }
+
         }
     }
 }
