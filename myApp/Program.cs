e83bfa93bbabcfcb6fc4b49c495d3c21e2b47bef
Rearles <53582049+Rearles@users.noreply.github.com>
2021-08-01 13:21:21 -0600

    Additions to track ccnum
    Continue to add to the atm code. it's coming slow

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -10,26 +10,44 @@ namespace myApp
         Dictionary<string, string> names = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Do you have an account with us?");
+            string account = Console.ReadLine();
+            if (account == "Yes" || account == "yes") {
+                
+            }
             bool cardnum = false;
             while (cardnum == false) {
                 Console.WriteLine("Enter your credit card number or debit card number");
-                string ccnum = Console.ReadLine();
-                if (ccnum.IndexOf(4) == 0) {
+                string ccnum = Console.ReadLine(); 
+                if (ccnum.IndexOf("4") == 0) {
                 Console.WriteLine("Your card is a Visa correct?");
                 string answer = Console.ReadLine();                
-                if (answer == "Yes" || answer == "yes") {
-                    Console.WriteLine("Thank you for confirming for me");
-                    cardnum = true;
-                } 
-                else if (answer == "No" || answer == "no") {
-                    Console.WriteLine("You entered the wrong information into the system. Please try again.");
-                } else {
-                    Console.WriteLine("You entered incorrect characters, try again");
+                    if (answer == "Yes" || answer == "yes") {
+                        Console.WriteLine("Thank you for confirming for me");
+                        cardnum = true;
+                    } 
+                    else if (answer == "No" || answer == "no") {
+                        Console.WriteLine("You entered the wrong information into the system. Please try again.");
+                    } else {
+                        Console.WriteLine("You entered incorrect characters, try again");
+                    }
+                } else if (ccnum.IndexOf("5") == 0 || ccnum.IndexOf("2") == 0) {
+                    Console.WriteLine("Your card is Mastercard right?");
+                    string answer = Console.ReadLine();
+                    if (answer == "Yes" || answer == "yes") {
+                        Console.WriteLine("Thank you for confirming for me");
+                        cardnum = true;
+                    } 
+                    else if (answer == "No" || answer == "no") {
+                        Console.WriteLine("You entered the wrong information into the system. Please try again.");
+                    } else {
+                        Console.WriteLine("You entered incorrect characters, try again");
+                    }
                 }
+
             }
-            
-            }
+
+
 
         }
     }
