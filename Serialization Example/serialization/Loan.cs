869d8159470d84aa59f1f2c5c98f43cb90ab56e8
Rearles <53582049+Rearles@users.noreply.github.com>
2021-08-01 09:52:59 -0700

    Current Attempt (See Desc)
    Attempt 1 at figuring out and making variables save themselves across instances of "dotnet runs", so that I can save things to my atm that last past the current iteration of the atm.

---
+++ Serialization Example/serialization/Loan.cs
@@ -0,0 +1,36 @@
+public class Loan : INotifyPropertyChanged
+ {
+     public double LoanAmount { get; set; }
+     public double InterestRatePercent { get; set; }
+
+     [field:NonSerialized()]
+     public DateTime TimeLastLoaded { get; set; }
+
+     public int Term { get; set; }
+
+     private string customer;
+     public string Customer
+     {
+         get { return customer; }
+         set
+         {
+             customer = value;
+             PropertyChanged?.Invoke(this,
+               new PropertyChangedEventArgs(nameof(Customer)));
+         }
+     }
+
+     [field: NonSerialized()]
+     public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+     public Loan(double loanAmount,
+                 double interestRate,
+                 int term,
+                 string customer)
+     {
+         this.LoanAmount = loanAmount;
+         this.InterestRatePercent = interestRate;
+         this.Term = term;
+         this.customer = customer;
+     }
+ }
---
+++ Serialization Example/serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loan TestLoan = new Loan(10000.0, 7.5, 36, "Neil Black");
+        }
+    }
+}
