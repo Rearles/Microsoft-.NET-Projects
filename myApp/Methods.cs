5607c500a3b2c957587a892143c53ec87f4e8114
Rearles <53582049+Rearles@users.noreply.github.com>
2021-08-12 12:14:29 -0600

    Adding summary documentation
    Adds documentation to explain what my methods do so that I understand what I was thinking today when I am programming in the future. And so people looking at my code understand what its trying to do even if it doesn't fully work yet.

--- myApp/Methods.cs
+++ myApp/Methods.cs
@@ -8,16 +8,44 @@ namespace myApp
     class Methods : bankAccount
     {
         private static Dictionary<string, List<bankAccount>> accountinformation = new Dictionary<string, List<bankAccount>>();
-        public static void bankAccountAdd(string name, bankAccount myaccount) {
-            accountinformation[name] = myaccount; //this should add the name associated with the bank account to a dictionary
+        /// <summary>
+        /// this method adds a bank account to the dictionary
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="myaccount"></param>
+        public static void bankAccountAdd(string name, bankAccount myaccount) { //adds bank account to the dictionary for the bank account myaccount to be accessible later.
+            int index = GetNums(name);
+            accountinformation[name].Add(myaccount[index]); //this should add the name associated with the bank account to a dictionary
         }
-        public static List<bankAccount> bankAccountRetrieve(string name) {
+        /// <summary>
+        /// retreives the bank account that should be in accountinformation dictionary, and if it doesn't exist it returns null
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>either List<bankAccount> at given name or null if it doesn't exist</returns>
+        public static List<bankAccount> bankAccountRetrieve(string name) { 
             if (accountinformation.ContainsKey(name)) { //if the name exists in the dictionary
                 return accountinformation[name];
             } else {
                 return null;
             }
         } 
+        /// <summary>
+        /// this allows me to access the amount of items in the dictionary at a given name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>index</returns>
+        public static int GetNums(string name) {
+            if (!accountinformation.ContainsKey(name)) {
+                return 0;
+            } else if (accountinformation.ContainsKey(name)) {
+                return accountinformation[name].Count() - 1;
+            }
+        }
+        /// <summary>
+        /// generates bank account number equal to length and returns that number to be used in bank account creation. 
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
         public static int RandomDigits(int length) //to create a new bank account number
         {
             var random = new Random();
